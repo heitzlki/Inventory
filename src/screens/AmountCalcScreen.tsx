85a14060df822d50493f7012812105195e9bd63e
heitzlki <kirill.heitzler@gmail.com>
2022-12-24 16:18:34 +0100

    wip screen

---
+++ src/screens/AmountCalcScreen.tsx
@@ -0,0 +1,333 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import type { StyleProp, ViewStyle } from 'react-native';
+
+import type { RootStackScreenProps } from 'navigation/types';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { itemAdd, itemDelete, itemSetAmount } from 'store/inventuries';
+import { RootState } from 'store/index';
+
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import BottomSheet, { BottomSheetRefProps } from 'components/BottomSheet';
+
+interface ButtonProps {
+  onPress: () => void;
+  title: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+const evalAndFormat = (input: string) => {
+  return eval(input.replace(/^0+(?=\d)/, ''))
+    .toFixed(3)
+    .replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1')
+    .toString();
+};
+
+const AmountCalcButton = ({ title, onPress, style }: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        {
+          width: 72,
+          height: 72,
+          borderRadius: 24,
+          backgroundColor: '#2f3136',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 5,
+        },
+        style,
+      ]}
+      onPress={onPress}>
+      <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 28 }}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const AmountCalcScreen = ({
+  route,
+  navigation,
+}: RootStackScreenProps<'AmountCalc'>) => {
+  const { inventuryId, itemId } = route.params;
+
+  const inventuries = useSelector(
+    (state: RootState) => state.inveturiesReducer,
+  );
+
+  const dispatch = useDispatch();
+
+  // const
+
+  // const displayFirstNumber = (): number => {
+  //   return
+  // }
+
+  const [amount, setAmount] = useState(
+    inventuries[inventuryId].items[itemId].amount,
+  );
+
+  const [confirmed, setConfirmed] = useState(true);
+
+  const displayFirstNumber = () => {
+    return amount;
+  };
+
+  const displaySecondNumber = () => {
+    try {
+      if (!confirmed && amount != '') {
+        return evalAndFormat(amount);
+      } else {
+        return '';
+      }
+    } catch {
+      return '';
+    }
+  };
+
+  useEffect(() => {
+    try {
+      if (evalAndFormat(amount) == amount) {
+        setConfirmed(true);
+      } else {
+        setConfirmed(false);
+      }
+    } catch {
+      setConfirmed(false);
+    }
+  }, [amount]);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#36393f' }}>
+      <View style={{ position: 'absolute', top: 20, width: '100%' }}>
+        <Text>{`${inventuryId}: ${inventuries[inventuryId].name}`}</Text>
+        <Text>{`${itemId}: ${inventuries[inventuryId].items[itemId].name}`}</Text>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          width: '100%',
+        }}>
+        <View style={{}}>
+          <View
+            style={{
+              alignItems: 'flex-end',
+              right: 48,
+            }}>
+            <Text style={{ color: '#EBECED', fontWeight: '500', fontSize: 56 }}>
+              {displayFirstNumber()}
+            </Text>
+
+            <Text style={{ color: '#EBECED', fontWeight: '500', fontSize: 34 }}>
+              {displaySecondNumber()}
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <AmountCalcButton
+            title="1"
+            onPress={() => {
+              setAmount(`${amount}1`);
+            }}
+          />
+          <AmountCalcButton
+            title="2"
+            onPress={() => {
+              setAmount(`${amount}2`);
+            }}
+          />
+          <AmountCalcButton
+            title="3"
+            onPress={() => {
+              setAmount(`${amount}3`);
+            }}
+          />
+          <AmountCalcButton
+            style={{ backgroundColor: '#292B2F' }}
+            title="÷"
+            onPress={() => {
+              setAmount(`${amount}/`);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <AmountCalcButton
+            title="4"
+            onPress={() => {
+              setAmount(`${amount}4`);
+            }}
+          />
+          <AmountCalcButton
+            title="5"
+            onPress={() => {
+              setAmount(`${amount}5`);
+            }}
+          />
+          <AmountCalcButton
+            title="6"
+            onPress={() => {
+              setAmount(`${amount}6`);
+            }}
+          />
+          <AmountCalcButton
+            style={{ backgroundColor: '#292B2F' }}
+            title="×"
+            onPress={() => {
+              setAmount(`${amount}*`);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <AmountCalcButton
+            title="7"
+            onPress={() => {
+              setAmount(`${amount}7`);
+            }}
+          />
+          <AmountCalcButton
+            title="8"
+            onPress={() => {
+              setAmount(`${amount}8`);
+            }}
+          />
+          <AmountCalcButton
+            title="9"
+            onPress={() => {
+              setAmount(`${amount}9`);
+            }}
+          />
+          <AmountCalcButton
+            style={{ backgroundColor: '#292B2F' }}
+            title="+"
+            onPress={() => {
+              setAmount(`${amount}+`);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <AmountCalcButton
+            title=","
+            onPress={() => {
+              setAmount(`${amount}.`);
+            }}
+          />
+          <AmountCalcButton
+            title="0"
+            onPress={() => {
+              setAmount(`${amount}0`);
+            }}
+          />
+          <AmountCalcButton
+            title="⌫"
+            onPress={() => {
+              setAmount(`${amount.substring(0, amount.length - 1)}`);
+            }}
+          />
+          <AmountCalcButton
+            style={{ backgroundColor: '#292B2F' }}
+            title="-"
+            onPress={() => {
+              setAmount(`${amount}-`);
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <TouchableOpacity
+            style={{
+              width: 313,
+              height: 72,
+              borderRadius: 24,
+              backgroundColor: '#202225',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 5,
+            }}
+            onPress={() => {
+              try {
+                if (amount != '') {
+                  setAmount(evalAndFormat(amount));
+                  if (confirmed) {
+                    dispatch(
+                      itemSetAmount({ inventuryId, itemId, newAmount: amount }),
+                    );
+                    navigation.goBack();
+                  } else {
+                    setConfirmed(true);
+                  }
+                }
+              } catch {}
+            }}>
+            <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 28 }}>
+              {confirmed ? '✅' : '='}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* <View
+          style={{
+            maxWidth: '100%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <TouchableOpacity
+            style={{
+              width: 154,
+              height: 72,
+              borderRadius: 24,
+              backgroundColor: '#2f3136',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 5,
+            }}>
+            <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 28 }}>
+              ✅
+            </Text>
+          </TouchableOpacity>
+        </View> */}
+      </View>
+    </View>
+  );
+};
+
+export default AmountCalcScreen;
