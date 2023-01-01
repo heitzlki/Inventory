8880a5ddbe65b4f0016dbbacf8366d2971077c5d
heitzlki <kirill.heitzler@gmail.com>
2023-01-01 05:33:56 +0100

    whole format, create, share xlsx sheet hook

---
+++ src/hooks/useFormatCreateShareXlsx/useFormatCreateShareXlsx.tsx
@@ -0,0 +1,38 @@
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/index';
+
+import { useCreateXlsxSheet } from 'hooks/useFormatCreateShareXlsx/useCreateXlsxSheet';
+import { useShareFile } from 'hooks/useFormatCreateShareXlsx/useShareFile';
+import { useFormatXlsx } from 'hooks/useFormatCreateShareXlsx/useFormatXlsx';
+import { ItemsState } from 'store/inventories/state';
+
+export function useFormatCreateAndShareXlsx(inventoryId: string) {
+  const inventories = useSelector(
+    (state: RootState) => state.invetoriesReducer,
+  );
+
+  const items: ItemsState | {} = inventories[inventoryId]?.items || {};
+
+  const { formattedData, formatData } = useFormatXlsx(items);
+
+  const {
+    createSheet,
+    filePath,
+    loading: creating,
+    error: createError,
+  } = useCreateXlsxSheet(inventoryId, formattedData);
+
+  const {
+    shareFile,
+    loading: sharing,
+    error: shareError,
+  } = useShareFile(filePath);
+
+  async function formatCreateAndShare() {
+    formatData();
+    await createSheet();
+    await shareFile();
+  }
+
+  return { formatCreateAndShare, creating, createError, sharing, shareError };
+}
