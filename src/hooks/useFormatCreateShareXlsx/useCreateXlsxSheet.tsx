27c3a6ea8751d173b2e4e8930d071b6df24852e5
heitzlki <kirill.heitzler@gmail.com>
2023-01-01 05:33:26 +0100

    create and save xlsx sheet in cache

---
+++ src/hooks/useFormatCreateShareXlsx/useCreateXlsxSheet.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import XLSX from 'xlsx';
+import RNFS from 'react-native-fs';
+import moment from 'moment';
+
+export function useCreateXlsxSheet(inventoryId: string, data: string[][]) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [filePath, setFilePath] = useState('');
+
+  async function createSheet() {
+    setLoading(true);
+    setError(null);
+    setFilePath('');
+
+    try {
+      // Create the xlsx sheet
+      const fileName = `${inventoryId}_${moment().unix().toString()}.xlsx`;
+      const filePathStr = `${RNFS.CachesDirectoryPath}/${fileName}`;
+
+      const ws = XLSX.utils.aoa_to_sheet(data);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+      const wbout = XLSX.write(wb, { type: 'binary', bookType: 'xlsx' });
+      await RNFS.writeFile(filePathStr, wbout, 'ascii').then(() => {
+        setFilePath(filePathStr);
+      });
+    } catch (e: any) {
+      setError(e);
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { createSheet, filePath, loading, error };
+}
