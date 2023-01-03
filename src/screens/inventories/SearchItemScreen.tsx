36c0fa89d5661b3917fa774f24e2115570a10bb6
heitzlki <kirill.heitzler@gmail.com>
2023-01-03 12:50:08 +0100

    optimize search algo and focus input on navigation

--- src/screens/inventories/SearchItemScreen.tsx
+++ src/screens/inventories/SearchItemScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import {
   Button,
   View,
@@ -12,6 +12,8 @@ import {
 
 import uuid from 'react-native-uuid';
 
+import { useFocusEffect } from '@react-navigation/native';
+
 import { inventoryItemAdd } from 'store/inventories';
 import type { ProductState } from 'store/catalog/state';
 import { useSelector, useDispatch } from 'react-redux';
@@ -20,6 +22,7 @@ import { RootState } from 'store/index';
 import type { RootStackScreenProps } from 'navigation/types';
 
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const SearchItemScreen = ({
   route,
@@ -35,19 +38,32 @@ const SearchItemScreen = ({
   const dispatch = useDispatch();
 
   function search(query: string): ProductState[] {
-    const results: ProductState[] = [];
-    for (const key in catalog) {
-      if (catalog[key].name.indexOf(query) !== -1) {
-        results.push(catalog[key]);
-      }
-    }
-    return results;
+    // Convert the search query to lowercase to make the search case-insensitive
+    query = query.toLowerCase();
+
+    // Filter the catalog by products whose names contain the search query
+    const results: ProductState[] = Object.values(catalog).filter(
+      product => product.name.toLowerCase().indexOf(query) !== -1,
+    );
+
+    // Sort the results by the product name
+    return results.sort((a, b) => a.name.localeCompare(b.name));
   }
 
   function handleSearchChange(query: string) {
     setSearchQuery(query);
     setSearchResults(search(query));
   }
+
+  const inputRef = useRef<TextInput>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 0);
+    }, [inputRef]),
+  );
 
   return (
     <View style={{ flex: 1, backgroundColor: '#36393f' }}>
@@ -66,25 +82,47 @@ const SearchItemScreen = ({
           flexDirection: 'row',
           alignItems: 'center',
         }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', left: 10 }}>
-          <Pressable style={{}} onPress={() => navigation.goBack()}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 8,
+          }}>
+          <Pressable onPress={() => navigation.goBack()}>
             <MaterialCommunityIcon
               name="keyboard-backspace"
               size={26}
               color="#DCDDDE"
             />
           </Pressable>
-          <TextInput
+          <View
             style={{
-              color: '#DCDDDE',
-              fontWeight: '500',
-              fontSize: 16,
-            }}
-            placeholderTextColor={'#BCBFC5'}
-            value={searchQuery}
-            onChangeText={handleSearchChange}
-            placeholder="Search"
-          />
+              flexDirection: 'row',
+              alignItems: 'center',
+              backgroundColor: '#202225',
+              paddingHorizontal: 8,
+              flex: 1,
+              borderRadius: 15,
+              height: 42,
+            }}>
+            <MaterialIcon name="search" size={26} color="#DCDDDE" />
+            <TextInput
+              ref={inputRef}
+              style={{
+                color: '#DCDDDE',
+                fontWeight: '500',
+                fontSize: 16,
+                flex: 1,
+              }}
+              placeholderTextColor={'#BCBFC5'}
+              value={searchQuery}
+              onChangeText={handleSearchChange}
+              placeholder="Search"
+            />
+            <Pressable onPress={() => setSearchQuery('')}>
+              <MaterialIcon name="clear" size={26} color="#DCDDDE" />
+            </Pressable>
+          </View>
         </View>
       </View>
 
@@ -113,7 +151,11 @@ const SearchItemScreen = ({
                 dispatch(inventoryItemAdd({ inventoryId, id: itemId }));
 
                 navigation.goBack();
-                navigation.navigate('AmountInput', { inventoryId, itemId });
+                navigation.navigate('AmountInput', {
+                  inventoryId,
+                  itemId,
+                  prediction: '',
+                });
               }}>
               <View
                 style={{
