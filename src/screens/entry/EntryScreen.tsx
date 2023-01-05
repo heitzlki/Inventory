534d67806371938046f5b4dd9ad31fa6f63d3c8a
heitzlki <kirill.heitzler@gmail.com>
2023-01-05 16:05:48 +0100

    refactor complete ui

--- src/screens/entry/EntryScreen.tsx
+++ src/screens/entry/EntryScreen.tsx
@@ -6,45 +6,75 @@ const EntryScreen = ({ navigation }: RootStackScreenProps<'Entry'>) => (
     <View
       style={{
         position: 'absolute',
-        top: 100,
+        zIndex: 2,
+        top: 0,
         width: '100%',
+        height: 58,
+
+        backgroundColor: '#292B2F',
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+        flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'center',
       }}>
-      <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 54 }}>
+      <Text
+        style={{
+          color: '#DCDDDE',
+          fontWeight: '500',
+          fontSize: 18,
+        }}>
         Inventory
       </Text>
     </View>
-    <View style={{ position: 'absolute', bottom: 84, width: '100%' }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-        <Pressable
+    <View style={{ top: 58, alignItems: 'center' }}>
+      <Pressable
+        style={{
+          height: 42,
+          width: '95%',
+          backgroundColor: '#2f3136',
+          marginVertical: 4,
+          borderRadius: 8,
+
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => {
+          navigation.navigate('SignIn');
+        }}>
+        <Text
           style={{
-            width: 124,
-            height: 54,
-            backgroundColor: '#292B2F',
-            borderRadius: 15,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          onPress={() => navigation.navigate('SignIn')}>
-          <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 24 }}>
-            Sign In
-          </Text>
-        </Pressable>
-        <Pressable
+            color: '#DCDDDE',
+            fontWeight: '500',
+            fontSize: 16,
+          }}>
+          Sign In
+        </Text>
+      </Pressable>
+
+      <Pressable
+        style={{
+          height: 42,
+          width: '95%',
+          backgroundColor: '#2f3136',
+          marginVertical: 4,
+          borderRadius: 8,
+
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => {
+          navigation.navigate('SignUp');
+        }}>
+        <Text
           style={{
-            width: 124,
-            height: 54,
-            backgroundColor: '#292B2F',
-            borderRadius: 15,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          onPress={() => navigation.navigate('SignUp')}>
-          <Text style={{ color: '#DCDDDE', fontWeight: '500', fontSize: 24 }}>
-            Sign Up
-          </Text>
-        </Pressable>
-      </View>
+            color: '#DCDDDE',
+            fontWeight: '500',
+            fontSize: 16,
+          }}>
+          Sign Up
+        </Text>
+      </Pressable>
     </View>
   </View>
 );
