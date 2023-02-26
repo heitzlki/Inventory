0ad9a9c2d0692477beae5bea636c4cae6ef24248
heitzlki <kirill.heitzler@gmail.com>
2023-02-26 13:34:02 +0100

    add theme, labels wip

--- client/Inventory/src/screens/inventories/SearchItemScreen.tsx
+++ client/Inventory/src/screens/inventories/SearchItemScreen.tsx
@@ -28,10 +28,17 @@ import type { RootStackScreenProps } from 'navigation/types';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
+import MyTopBar from 'components/custom/MyTopBar';
+import MyAddButton from 'components/custom/MyAddButton';
+import MyPressableIcon from 'components/custom/MyPressableIcon';
+import MyIcon from 'components/custom/MyIcon';
+import MyText from 'components/custom/MyText';
+
 const SearchItemScreen = ({
   route,
   navigation,
 }: RootStackScreenProps<'SearchItem'>) => {
+  const theme = useSelector((state: RootState) => state.themeReducer);
   const { inventoryId } = route.params;
 
   const [searchQuery, setSearchQuery] = useState('');
@@ -71,168 +78,191 @@ const SearchItemScreen = ({
     }, [inputRef]),
   );
 
+  useEffect(() => {
+    setSearchResults(Object.values(catalog));
+  }, [catalog]);
+
+  const [topBarHeight, setTopBarHeight] = useState<number>();
+
   return (
     <MyBackground>
-      <View
-        style={{
-          position: 'absolute',
-          zIndex: 2,
-          top: 0,
-          width: '100%',
-          height: 58,
-
-          backgroundColor: '#292B2F',
-          borderBottomLeftRadius: 15,
-          borderBottomRightRadius: 15,
-          // justifyContent: 'center',
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}>
+      <MyTopBar backButton={true} title="" style={{ height: topBarHeight }}>
         <View
+          onLayout={event => {
+            const { height } = event.nativeEvent.layout;
+            setTopBarHeight(height);
+          }}
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingHorizontal: 8,
+            flexDirection: 'column',
+            position: 'relative',
+            width: '100%',
+            paddingHorizontal: 10,
           }}>
-          <Pressable
-            style={{ marginHorizontal: 10 }}
-            onPress={() => navigation.goBack()}>
-            <MaterialCommunityIcon
-              name="keyboard-backspace"
-              size={26}
-              color="#DCDDDE"
-            />
-          </Pressable>
           <View
             style={{
               flexDirection: 'row',
               alignItems: 'center',
-              backgroundColor: '#202225',
+              backgroundColor: theme.style.colorSix,
               paddingHorizontal: 8,
+              marginRight: 20,
               flex: 1,
               borderRadius: 15,
-              height: 42,
+              maxHeight: 42,
+              minHeight: 42,
             }}>
-            <MaterialIcon name="search" size={26} color="#DCDDDE" />
+            <MyIcon set="MaterialIcons" name="search" size={26} />
             <TextInput
-              ref={inputRef}
               style={{
-                color: '#DCDDDE',
+                color: theme.style.text,
                 fontWeight: '500',
                 fontSize: 16,
                 flex: 1,
               }}
-              placeholderTextColor={'#ABB0B6'}
+              ref={inputRef}
+              placeholderTextColor={theme.style.textDim}
               value={searchQuery}
               onChangeText={handleSearchChange}
               placeholder="Search"
             />
-            <Pressable onPress={() => setSearchQuery('')}>
-              <MaterialIcon name="clear" size={26} color="#DCDDDE" />
-            </Pressable>
+            <MyPressableIcon
+              style={{ paddingHorizontal: 8 }}
+              onPress={() => {
+                setSearchQuery('');
+                setSearchResults(Object.values(catalog));
+              }}
+              set="MaterialIcons"
+              name="clear"
+              size={26}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              marginRight: 20,
+              marginVertical: 4,
+            }}>
+            {[
+              'Aktionsprodukte',
+              'Frisch- und TK-Ware (1)',
+              'Frisch- und TK-Ware (2)',
+              'Getränke',
+              'Soßen, Dips und Dressings',
+              'Desserts (TK)',
+              'Dosen- und Trockenware',
+              'Verpackungen',
+            ].map((item, index) => (
+              <View
+                key={index}
+                style={{
+                  paddingHorizontal: 8,
+                  margin: 2,
+                  borderRadius: 5,
+                  backgroundColor: '#00ffcca7',
+                }}>
+                <MyText text={item} />
+              </View>
+            ))}
           </View>
         </View>
-      </View>
+      </MyTopBar>
+      <FlatList
+        contentContainerStyle={{
+          alignItems: 'center',
+          paddingBottom: 80, // Bottom space for add button
+        }}
+        data={searchResults}
+        renderItem={({ item, index }) => (
+          <Pressable
+            key={item.id}
+            style={{
+              height: 50,
+              minWidth: '95%',
+              backgroundColor: '#2f3136',
+              marginVertical: 4,
+              borderRadius: 8,
 
-      <View style={{ flex: 1, paddingTop: 58, justifyContent: 'center' }}>
-        <FlatList
-          contentContainerStyle={{
-            alignItems: 'center',
-            paddingBottom: 80, // Bottom space for add button
-          }}
-          data={searchResults}
-          renderItem={({ item, index }) => (
-            <Pressable
-              key={item.id}
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              dispatch(
+                inventoryItemAdd({
+                  inventoryId,
+                  productId: item.id,
+                  name: item.name,
+                  amountType: item.amountType,
+                }),
+              );
+              const newItemId = Object.keys(
+                store.getState().invetoriesReducer[inventoryId].items,
+              )[0];
+
+              navigation.goBack();
+              navigation.navigate('AmountInput', {
+                inventoryId,
+                itemId: newItemId,
+                prediction: item.defaultAmount == '0' ? '' : item.defaultAmount,
+              });
+            }}>
+            <View
               style={{
-                height: 50,
-                minWidth: '95%',
-                backgroundColor: '#2f3136',
-                marginVertical: 4,
-                borderRadius: 8,
-
+                left: 10,
                 flexDirection: 'row',
                 alignItems: 'center',
-              }}
-              onPress={() => {
-                dispatch(
-                  inventoryItemAdd({
-                    inventoryId,
-                    productId: item.id,
-                    name: item.name,
-                  }),
-                );
-                const newItemId = Object.keys(
-                  store.getState().invetoriesReducer[inventoryId].items,
-                )[0];
-
-                navigation.goBack();
-                navigation.navigate('AmountInput', {
-                  inventoryId,
-                  itemId: newItemId,
-                  prediction:
-                    item.defaultAmount == '0' ? '' : item.defaultAmount,
-                });
               }}>
-              <View
+              <MaterialCommunityIcon name="leaf" size={24} color="#98f5e1" />
+              <Text
                 style={{
-                  left: 10,
-                  flexDirection: 'row',
-                  alignItems: 'center',
+                  color: '#DCDDDE',
+                  fontWeight: '500',
+                  fontSize: 16,
+                  left: 4,
                 }}>
-                <MaterialCommunityIcon name="leaf" size={24} color="#98f5e1" />
-                <Text
-                  style={{
-                    color: '#DCDDDE',
-                    fontWeight: '500',
-                    fontSize: 16,
-                    left: 4,
-                  }}>
-                  {item.name}
-                </Text>
-              </View>
-              <View
+                {item.name}
+              </Text>
+            </View>
+            <View
+              style={{
+                position: 'absolute',
+                right: 0,
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginHorizontal: 10,
+              }}>
+              <Text
                 style={{
-                  position: 'absolute',
-                  right: 0,
-                  flexDirection: 'row',
-                  alignItems: 'center',
+                  color: '#ABB0B6',
+                  fontWeight: '500',
+                  fontSize: 16,
                   marginHorizontal: 10,
                 }}>
-                <Text
-                  style={{
-                    color: '#ABB0B6',
-                    fontWeight: '500',
-                    fontSize: 16,
-                    marginHorizontal: 10,
-                  }}>
-                  {item.defaultAmount}
-                </Text>
-                <Text
-                  style={{
-                    color: '#ABB0B6',
-                    fontWeight: '500',
-                    fontSize: 16,
-                  }}>
-                  {item.unit}
-                </Text>
-              </View>
-            </Pressable>
-          )}
-          ListFooterComponent={
-            <Text
-              style={{
-                color: '#ABB0B6',
-                fontWeight: '500',
-                fontSize: 16,
-                marginVertical: 10,
-              }}
-              onPress={() => navigation.navigate('Catalog')}>
-              Create new
-            </Text>
-          }
-        />
-      </View>
+                {item.defaultAmount}
+              </Text>
+              <Text
+                style={{
+                  color: '#ABB0B6',
+                  fontWeight: '500',
+                  fontSize: 16,
+                }}>
+                {item.unit}
+              </Text>
+            </View>
+          </Pressable>
+        )}
+        ListFooterComponent={
+          <MyText
+            style={{
+              fontWeight: '500',
+              fontSize: 16,
+              marginVertical: 10,
+            }}
+            onPress={() => navigation.navigate('Catalog')}
+            text={'Create New'}
+          />
+        }
+      />
     </MyBackground>
   );
 };
