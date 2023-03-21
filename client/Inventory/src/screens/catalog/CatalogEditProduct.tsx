418fc36cb4d08935d521c6c5bfeebd59784b9b15
heitzlki <kirill.heitzler@gmail.com>
2023-03-22 00:33:46 +0100

    almost finished wip edit product

--- client/Inventory/src/screens/catalog/CatalogEditProduct.tsx
+++ client/Inventory/src/screens/catalog/CatalogEditProduct.tsx
@@ -5,8 +5,9 @@ import {
   TouchableOpacity,
   Pressable,
   TextInput,
+  TextInputProps,
+  FlatList,
 } from 'react-native';
-import type { StyleProp, ViewStyle } from 'react-native';
 
 import type { RootStackScreenProps } from 'navigation/types';
 
@@ -19,7 +20,67 @@ import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIc
 import BottomSheet, { BottomSheetRefProps } from 'components/BottomSheet';
 import { ProductState } from 'store/catalog/state';
 
-import { MyBackground } from 'components/custom';
+import {
+  MyBackground,
+  MyTopBar,
+  MyButton,
+  MyText,
+  MyCategoryLabel,
+} from 'components/custom';
+import { validUnits, validAmounts, validCategories } from 'store/catalog/state';
+import { useGoBack } from 'hooks/useGoBack';
+
+interface CatalogEditProductTextInputProps extends TextInputProps {
+  name?: string;
+}
+
+const CatalogEditProductTextInput = ({
+  name = '',
+  value,
+  onChange,
+  ...props
+}: CatalogEditProductTextInputProps) => {
+  const theme = useSelector((state: RootState) => state.themeReducer);
+
+  return (
+    <View
+      style={{
+        height: 42,
+        width: '95%',
+        backgroundColor: theme.style.colorFour,
+        marginVertical: 4,
+        borderRadius: 8,
+
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      {name ? (
+        <MyText
+          style={{
+            marginLeft: 8,
+            color: theme.style.text,
+            fontWeight: '500',
+            fontSize: 16,
+          }}
+          text={name}
+        />
+      ) : null}
+      <TextInput
+        style={{
+          marginLeft: 4,
+          color: theme.style.text,
+          fontWeight: '500',
+          fontSize: 16,
+          flex: 1,
+        }}
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+    </View>
+  );
+};
 
 const CatalogEditProductScreen = ({
   route,
@@ -27,33 +88,51 @@ const CatalogEditProductScreen = ({
 }: RootStackScreenProps<'CatalogEditProduct'>) => {
   const { productId } = route.params;
 
+  const dispatch = useDispatch();
+
   const catalog = useSelector((state: RootState) => state.catalogReducer);
+
+  const theme = useSelector((state: RootState) => state.themeReducer);
 
   const product: ProductState = catalog[productId];
 
   const [editProduct, setEditProduct] = useState({
+    newProductId: product.id,
     name: product.name,
-    defaultAmountOne: product.defaultAmountOne,
     unit: product.unit,
+    amountType: product.amountType,
+    defaultAmountOne: product.defaultAmountOne,
+    defaultAmountTwo: product.defaultAmountTwo,
+    category: product.category,
   });
 
   const [valid, setValid] = useState(true);
 
-  const dispatch = useDispatch();
-
-  const editProductValid = (): boolean => {
-    if (!editProduct.name || editProduct.name.length === 0) {
-      return false;
-    }
-
-    if (!/^[-+]?\d*\.?\d+$/.test(editProduct.defaultAmountOne)) {
-      return false;
-    }
+  const validateEditProduct = (): boolean => {
+    const {
+      newProductId,
+      name,
+      unit,
+      amountType,
+      defaultAmountOne,
+      defaultAmountTwo,
+      category,
+    } = editProduct;
 
     if (
-      editProduct.unit !== 'pcs' &&
-      editProduct.unit !== 'g' &&
-      editProduct.unit !== 'kg'
+      typeof newProductId !== 'string' ||
+      newProductId.trim() === '' ||
+      typeof name !== 'string' ||
+      name.trim() === '' ||
+      !validUnits.includes(unit) ||
+      !validAmounts.includes(amountType) ||
+      typeof defaultAmountOne !== 'string' ||
+      defaultAmountOne.trim() === '' ||
+      isNaN(Number(defaultAmountOne)) ||
+      typeof defaultAmountTwo !== 'string' ||
+      defaultAmountTwo.trim() === '' ||
+      isNaN(Number(defaultAmountTwo)) ||
+      !validCategories.includes(category)
     ) {
       return false;
     }
@@ -62,7 +141,7 @@ const CatalogEditProductScreen = ({
   };
 
   useEffect(() => {
-    if (editProductValid()) {
+    if (validateEditProduct()) {
       setValid(true);
     } else {
       setValid(false);
@@ -71,45 +150,43 @@ const CatalogEditProductScreen = ({
 
   return (
     <MyBackground>
-      <View
-        style={{
-          position: 'absolute',
-          zIndex: 2,
-          top: 0,
-          width: '100%',
-          height: 58,
-
-          backgroundColor: '#292B2F',
-          borderBottomLeftRadius: 15,
-          borderBottomRightRadius: 15,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', left: 10 }}>
-          <Pressable style={{}} onPress={() => navigation.goBack()}>
-            <MaterialCommunityIcon
-              name="keyboard-backspace"
-              size={26}
-              color="#DCDDDE"
-            />
-          </Pressable>
-          <Text
-            style={{
-              color: '#DCDDDE',
-              fontWeight: '500',
-              fontSize: 16,
-              left: 4,
-            }}>
-            Edit Product
-          </Text>
-        </View>
-      </View>
-      <View style={{ top: 58, alignItems: 'center' }}>
+      <MyTopBar backButton={true} title="Settings" />
+      <View style={{ alignItems: 'center' }}>
+        <CatalogEditProductTextInput
+          name="ID:"
+          value={editProduct.newProductId}
+          onChangeText={text => {
+            setEditProduct({ ...editProduct, newProductId: text });
+          }}
+        />
+        <CatalogEditProductTextInput
+          name="Name:"
+          value={editProduct.name}
+          onChangeText={text => {
+            setEditProduct({ ...editProduct, name: text });
+          }}
+        />
+        <CatalogEditProductTextInput
+          name="Amount One:"
+          value={editProduct.defaultAmountOne}
+          onChangeText={text => {
+            setEditProduct({ ...editProduct, defaultAmountOne: text });
+          }}
+        />
+        {editProduct.amountType === 'double' ? (
+          <CatalogEditProductTextInput
+            name="Amount Two:"
+            value={editProduct.defaultAmountTwo}
+            onChangeText={text => {
+              setEditProduct({ ...editProduct, defaultAmountTwo: text });
+            }}
+          />
+        ) : null}
         <View
           style={{
             height: 42,
             width: '95%',
-            backgroundColor: '#2f3136',
+            backgroundColor: theme.style.colorFour,
             marginVertical: 4,
             borderRadius: 8,
 
@@ -117,224 +194,199 @@ const CatalogEditProductScreen = ({
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <TextInput
+          <MyText
             style={{
-              marginLeft: 4,
-              color: '#DCDDDE',
               fontWeight: '500',
               fontSize: 16,
               flex: 1,
-            }}
-            value={editProduct.name}
-            onChangeText={text => {
-              setEditProduct({ ...editProduct, name: text });
-            }}
-          />
-        </View>
-        <View
-          style={{
-            height: 42,
-            width: '95%',
-            backgroundColor: '#2f3136',
-            marginVertical: 4,
-            borderRadius: 8,
-
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <TextInput
-            style={{
-              marginLeft: 4,
-              color: '#DCDDDE',
-              fontWeight: '500',
-              fontSize: 16,
-              flex: 1,
-            }}
-            value={editProduct.defaultAmountOne}
-            onChangeText={text => {
-              setEditProduct({ ...editProduct, defaultAmountOne: text });
-            }}
-          />
-        </View>
-        <View
-          style={{
-            height: 42,
-            width: '95%',
-            backgroundColor: '#2f3136',
-            marginVertical: 4,
-            borderRadius: 8,
-
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
               marginLeft: 8,
-              color: '#DCDDDE',
-              fontWeight: '500',
-              fontSize: 16,
-              flex: 1,
-            }}>
-            Unit:
-          </Text>
+            }}
+            text="Unit:"
+          />
+
           <View
             style={{
-              position: 'absolute',
-              right: 0,
-              flexDirection: 'row',
-              alignItems: 'center',
+              maxWidth: 200,
             }}>
-            <Pressable
-              style={{
-                flex: 1,
-                margin: 5,
-                backgroundColor: '#292B2F',
+            <FlatList
+              horizontal
+              data={validUnits}
+              renderItem={({ item }) => (
+                <Pressable
+                  key={item}
+                  style={{
+                    flex: 1,
+                    margin: 5,
+                    backgroundColor: theme.style.colorSix,
 
-                paddingVertical: 5,
-                paddingHorizontal: 10,
+                    paddingHorizontal: 10,
 
-                borderRadius: 8,
-                borderColor: '#DCDDDE',
-                borderWidth: editProduct.unit == 'pcs' ? 2 : 0,
+                    borderRadius: 8,
+                    borderColor: theme.style.text,
+                    borderWidth: editProduct.unit == item ? 2 : 0,
 
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-              onPress={() => {
-                setEditProduct({ ...editProduct, unit: 'pcs' });
-              }}>
-              <Text
-                style={{
-                  color: editProduct.unit == 'pcs' ? '#DCDDDE' : '#ABB0B6',
-                  fontWeight: '500',
-                  fontSize: 16,
-                  textAlign: 'center',
-                }}>
-                pcs
-              </Text>
-            </Pressable>
-            <Pressable
-              style={{
-                flex: 1,
-                margin: 5,
-                backgroundColor: '#292B2F',
-
-                paddingVertical: 5,
-                paddingHorizontal: 10,
-
-                borderRadius: 8,
-                borderColor: '#DCDDDE',
-                borderWidth: editProduct.unit == 'g' ? 2 : 0,
-
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-              onPress={() => {
-                setEditProduct({ ...editProduct, unit: 'g' });
-              }}>
-              <Text
-                style={{
-                  color: editProduct.unit == 'g' ? '#DCDDDE' : '#ABB0B6',
-                  fontWeight: '500',
-                  fontSize: 16,
-                  textAlign: 'center',
-                }}>
-                g
-              </Text>
-            </Pressable>
-            <Pressable
-              style={{
-                flex: 1,
-                margin: 5,
-                backgroundColor: '#292B2F',
-
-                paddingVertical: 5,
-                paddingHorizontal: 10,
-
-                borderRadius: 8,
-                borderColor: '#DCDDDE',
-                borderWidth: editProduct.unit == 'kg' ? 2 : 0,
-
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-              onPress={() => {
-                setEditProduct({ ...editProduct, unit: 'kg' });
-              }}>
-              <Text
-                style={{
-                  color: editProduct.unit == 'kg' ? '#DCDDDE' : '#ABB0B6',
-                  fontWeight: '500',
-                  fontSize: 16,
-                  textAlign: 'center',
-                }}>
-                kg
-              </Text>
-            </Pressable>
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                  onPress={() => {
+                    setEditProduct({ ...editProduct, unit: item });
+                  }}>
+                  <MyText
+                    text={item}
+                    style={{
+                      fontWeight: '500',
+                      fontSize: 16,
+                      textAlign: 'center',
+                      color:
+                        editProduct.unit == item
+                          ? theme.style.text
+                          : theme.style.textDim,
+                    }}
+                  />
+                </Pressable>
+              )}
+            />
           </View>
         </View>
-        <Pressable
+        <View
           style={{
             height: 42,
             width: '95%',
-            backgroundColor: '#202225',
+            backgroundColor: theme.style.colorFour,
             marginVertical: 4,
             borderRadius: 8,
 
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'center',
-          }}
+          }}>
+          <MyText
+            style={{
+              fontWeight: '500',
+              fontSize: 16,
+              flex: 1,
+              marginLeft: 8,
+            }}
+            text="Amount Type:"
+          />
+          <View
+            style={{
+              maxWidth: 200,
+            }}>
+            <FlatList
+              horizontal
+              data={validAmounts}
+              renderItem={({ item }) => (
+                <Pressable
+                  key={item}
+                  style={{
+                    flex: 1,
+                    margin: 5,
+                    backgroundColor: theme.style.colorSix,
+
+                    paddingHorizontal: 10,
+
+                    borderRadius: 8,
+                    borderColor: theme.style.text,
+                    borderWidth: editProduct.amountType == item ? 2 : 0,
+
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                  onPress={() => {
+                    setEditProduct({
+                      ...editProduct,
+                      amountType: item,
+                      defaultAmountTwo: '0',
+                    });
+                  }}>
+                  <MyText
+                    text={item}
+                    style={{
+                      fontWeight: '500',
+                      fontSize: 16,
+                      textAlign: 'center',
+                      color:
+                        editProduct.amountType == item
+                          ? theme.style.text
+                          : theme.style.textDim,
+                    }}
+                  />
+                </Pressable>
+              )}
+            />
+          </View>
+        </View>
+
+        <MyButton
+          onPress={() => {}}
+          style={{
+            marginVertical: 4,
+            backgroundColor: theme.style.colorFour,
+          }}>
+          <MyText
+            style={{
+              fontWeight: '500',
+              fontSize: 16,
+              flex: 1,
+              marginLeft: 8,
+            }}
+            text="Category:"
+          />
+          <MyCategoryLabel
+            category={editProduct.category}
+            style={{ margin: 5 }}
+          />
+        </MyButton>
+        <MyButton
           onPress={() => {
             if (valid) {
-              dispatch(
-                catalogProductEdit({
-                  productId: product.id,
-                  name: editProduct.name,
-                  defaultAmountOne: editProduct.defaultAmountOne,
-                  unit: editProduct.unit,
-                }),
-              );
+              dispatch(catalogProductEdit({ productId, ...editProduct }));
               navigation.goBack();
             }
+          }}
+          style={{
+            marginVertical: 4,
+            justifyContent: 'center',
+            backgroundColor: theme.style.colorSix,
           }}>
-          <Text
+          <MyText
             style={{
-              color: valid ? '#98f5e1' : '#ABB0B6',
+              color: valid ? theme.style.colorGreen : theme.style.textDim,
               fontWeight: '500',
               fontSize: 18,
-            }}>
-            Save
-          </Text>
-        </Pressable>
-        <Pressable
-          style={{
-            height: 42,
-            width: '95%',
-            backgroundColor: '#202225',
-            marginVertical: 4,
-            borderRadius: 8,
-
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
+            }}
+            text="Save"
+          />
+        </MyButton>
+        <MyButton
           onPress={() => {
             dispatch(catalogProductDelete({ productId: product.id }));
             navigation.goBack();
+          }}
+          style={{
+            marginVertical: 4,
+            justifyContent: 'center',
+            backgroundColor: theme.style.colorSix,
           }}>
-          <Text
+          <MyText
             style={{
-              color: '#ff4d6d',
+              color: theme.style.colorRed,
               fontWeight: '500',
               fontSize: 18,
-            }}>
-            Delete
-          </Text>
-        </Pressable>
+            }}
+            text="Delete"
+          />
+        </MyButton>
       </View>
+
+      {/* 
+      // TODO
+      <InventoryBottomSheet
+        inventoryId={bottomSheetInventoryId}
+        bottomSheetRef={bottomSheetRef}
+        editNameRef={editNameRef}
+      /> */}
     </MyBackground>
   );
 };
