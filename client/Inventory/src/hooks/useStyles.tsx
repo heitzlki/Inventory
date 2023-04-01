79f9397c14db6b3373851777b2471d2662424c4f
heitzlki <kirill.heitzler@gmail.com>
2023-04-01 09:51:23 +0200

    wip

--- client/Inventory/src/hooks/useStyles.tsx
+++ client/Inventory/src/hooks/useStyles.tsx
@@ -9,64 +9,70 @@ import { ThemeState } from 'store/theme/state';
 
 type CategoryColorType = Record<
   CategoryType,
-  { colorOne: string; colorTwo: string }
+  { primary: string; secondary: string }
 >;
 
-export const categoryColors: CategoryColorType = {
-  Aktionsprodukte: { colorOne: '#02ab8ad6', colorTwo: '#27fdd4ff' },
-  '(1) Frisch- & TK-Ware': { colorOne: '#ab0241d6', colorTwo: '#ff0061ff' },
-  '(2) Frisch- & TK-Ware': { colorOne: '#93ab02d6', colorTwo: '#d1f004ff' },
-  'Soßen, Dips & Dressings': { colorOne: '#02ab46d6', colorTwo: '#00ff66ff' },
-  'Dosen- & Trockenware': { colorOne: '#028bacd6', colorTwo: '#55ddffff' },
-  Getränke: { colorOne: '#7002abd6', colorTwo: '#9c00f0ff' },
-  Verpackungen: { colorOne: '#4602acd6', colorTwo: '#6600ffff' },
-  'Desserts (TK)': { colorOne: '#575757d6', colorTwo: '#808080ff' },
+export const paletteCategory: CategoryColorType = {
+  Aktionsprodukte: { primary: '#02ab8ad6', secondary: '#27fdd4ff' },
+  '(1) Frisch- & TK-Ware': { primary: '#ab0241d6', secondary: '#ff0061ff' },
+  '(2) Frisch- & TK-Ware': { primary: '#93ab02d6', secondary: '#d1f004ff' },
+  'Soßen, Dips & Dressings': { primary: '#02ab46d6', secondary: '#00ff66ff' },
+  'Dosen- & Trockenware': { primary: '#028bacd6', secondary: '#55ddffff' },
+  Getränke: { primary: '#7002abd6', secondary: '#9c00f0ff' },
+  Verpackungen: { primary: '#4602acd6', secondary: '#6600ffff' },
+  'Desserts (TK)': { primary: '#575757d6', secondary: '#808080ff' },
 };
 
 export const darkTheme: Style = {
-  text: '#DCDDDE',
-  textDim: '#ABB0B6',
-  colorOne: '#72767D',
-  colorTwo: '#3B3E45',
-  colorThree: '#36393F',
-  colorFour: '#2F3136',
-  colorFive: '#292B2F',
-  colorSix: '#202225',
-  colorBlue: '#c1d3fe',
-  colorGreen: '#98f5e1',
-  colorRed: '#ff4d6d',
-  categoryColors,
+  colors: {
+    paletteTextMain: '#DCDDDE',
+    paletteTextLight: '#ABB0B6',
+    paletteOne: '#72767D',
+    paletteTwo: '#3B3E45',
+    paletteThree: '#36393F',
+    paletteFour: '#2F3136',
+    paletteFive: '#292B2F',
+    paletteSix: '#202225',
+    palettePrimaryBlue: '#c1d3fe',
+    palettePrimaryGreen: '#98f5e1',
+    palettePrimaryRed: '#ff4d6d',
+    paletteCategory,
+  },
 };
 
 export const lightTheme: Style = {
-  text: '#232221',
-  textDim: '#544f49',
-  colorOne: '#8d8982',
-  colorTwo: '#c4c1ba',
-  colorThree: '#c9c6c0',
-  colorFour: '#d0cec9',
-  colorFive: '#d6d4d0',
-  colorSix: '#dfddda',
-  colorBlue: '#4848ff',
-  colorGreen: '#00e9a8ff',
-  colorRed: '#ff274eff',
-  categoryColors,
+  colors: {
+    paletteTextMain: '#232221',
+    paletteTextLight: '#544f49',
+    paletteOne: '#8d8982',
+    paletteTwo: '#c4c1ba',
+    paletteThree: '#c9c6c0',
+    paletteFour: '#d0cec9',
+    paletteFive: '#d6d4d0',
+    paletteSix: '#dfddda',
+    palettePrimaryBlue: '#4848ff',
+    palettePrimaryGreen: '#00e9a8ff',
+    palettePrimaryRed: '#ff274eff',
+    paletteCategory,
+  },
 };
 
 export interface Style {
-  text: string;
-  textDim: string;
-  colorOne: string;
-  colorTwo: string;
-  colorThree: string;
-  colorFour: string;
-  colorFive: string;
-  colorSix: string;
-  colorBlue: string;
-  colorGreen: string;
-  colorRed: string;
-  categoryColors: {
-    [key in CategoryType]: { colorOne: string; colorTwo: string };
+  colors: {
+    paletteTextMain: string;
+    paletteTextLight: string;
+    paletteOne: string;
+    paletteTwo: string;
+    paletteThree: string;
+    paletteFour: string;
+    paletteFive: string;
+    paletteSix: string;
+    palettePrimaryBlue: string;
+    palettePrimaryGreen: string;
+    palettePrimaryRed: string;
+    paletteCategory: {
+      [key in CategoryType]: { primary: string; secondary: string };
+    };
   };
 }
 
