fe5d77154d79705c3090363c078ba7f89ebd2257
heitzlki <kirill.heitzler@gmail.com>
2023-04-03 09:25:04 +0200

    wip lang hook translations

--- client/Inventory/src/hooks/useLang.tsx
+++ client/Inventory/src/hooks/useLang.tsx
@@ -1,63 +1,36 @@
-import { useState } from 'react';
-
-// import { CategoryType } from 'store/catalog/state';
-
 import { useDispatch, useSelector } from 'react-redux';
-import { setTheme } from 'store/theme';
-import { RootState } from 'store/index';
-import { LangState } from 'store/lang/state';
+import { LangState, Lang } from 'store/lang/state';
+import { setLang } from 'store/lang';
 
-// type CategoryColorType = Record<
-//   CategoryType,
-//   { primary: string; secondary: string }
-// >;
-
-// export const paletteCategory: CategoryColorType = {
-//   Aktionsprodukte: { primary: '#02ab8ad6', secondary: '#27fdd4ff' },
-//   '(1) Frisch- & TK-Ware': { primary: '#ab0241d6', secondary: '#ff0061ff' },
-//   '(2) Frisch- & TK-Ware': { primary: '#93ab02d6', secondary: '#d1f004ff' },
-//   'Soßen, Dips & Dressings': { primary: '#02ab46d6', secondary: '#00ff66ff' },
-//   'Dosen- & Trockenware': { primary: '#028bacd6', secondary: '#55ddffff' },
-//   Getränke: { primary: '#7002abd6', secondary: '#9c00f0ff' },
-//   Verpackungen: { primary: '#4602acd6', secondary: '#6600ffff' },
-//   'Desserts (TK)': { primary: '#575757d6', secondary: '#808080ff' },
-// };
-
-export const langEn: Lang = {
-
+export const langEn: Translation = {
+  Hello: 'Hello',
 };
 
-export const langDe: Lang = {
-
+export const langDe: Translation = {
+  Hello: 'Hallo',
 };
 
-export interface Lang {
-
-    // categories: {
-    //   [key in CategoryType]: { primary: string; secondary: string };
-    // };
-  
+export interface Translation {
+  Hello: string;
 }
 
-export const useLang = () => {
-  const storeLang = useSelector((state: RootState) => state.langReducer);
+interface UseLang {
+  lang: Lang;
+  translations: Translation;
+  setLang: (lang: Lang) => void;
+}
 
+export const useLang = (): UseLang => {
+  const storeLang = useSelector(
+    (state: { langReducer: LangState }) => state.langReducer.lang,
+  );
   const dispatch = useDispatch();
 
-  const [lang, setLang] = useState<Lang>(storeLang.lang ?? 'en_EN');
+  const translations = storeLang === 'de_DE' ? langDe : langEn;
 
-  const changeLang = ({ lang }: LangState) => {
-    switch (lang) {
-      case 'en_EN':
-        dispatch(setLang({ lang: 'en_EN' }));
-        setLang(langEn);
-        break;
-      case 'de_DE':
-        dispatch(setLang({ lang: 'de_DE' }));
-        setLang(langDe);
-        break;
-      default:
+  const setLangWrapper = (lang: Lang) => {
+    dispatch(setLang({ lang }));
   };
 
-  return { lang, changeLang };
+  return { lang: storeLang, translations, setLang: setLangWrapper };
 };
