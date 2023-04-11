3b5d2893904ee7c0d9dd4baf40276bd2c89813c4
heitzlki <kirill.heitzler@gmail.com>
2023-04-11 12:10:39 +0200

    update types for amount input screen

--- client/Inventory/src/navigation/types.ts
+++ client/Inventory/src/navigation/types.ts
@@ -1,11 +1,18 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { EditingAmountEnum } from 'screens/inventories/AmountInputScreen';
 
 export type AppStackParamList = {
   Main: undefined;
   Inventory: { inventoryId: string };
   Settings: undefined;
   SearchItem: { inventoryId: string };
-  AmountInput: { inventoryId: string; itemId: string; prediction?: string };
+  AmountInput: {
+    inventoryId: string;
+    itemId: string;
+    amountOnePrediction?: string;
+    amounTwoPrediction?: string;
+    selectedAmount?: EditingAmountEnum;
+  };
   Catalog: undefined;
   CatalogEditProduct: { productId: string };
   Reminder: undefined;
