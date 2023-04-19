4188390ad0ac1146ea96748f9b35788003fa082b
heitzlki <kirill.heitzler@gmail.com>
2023-04-19 22:18:58 +0200

    export hooks

--- client/Inventory/src/hooks/useFormatCreateShareXlsx.tsx
+++ client/Inventory/src/hooks/useFormatCreateShareXlsx.tsx
@@ -1,65 +1,109 @@
+import { Buffer } from 'buffer';
+import moment from 'moment';
 import { PermissionsAndroid } from 'react-native';
-
+import RNFS from 'react-native-fs';
 import Share from 'react-native-share';
 import XLSX from 'xlsx';
-import RNFS from 'react-native-fs';
-import moment from 'moment';
 
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/index';
 
 import { ItemsState } from 'store/inventories/state';
 
-const sheetStructure = [['name', 'amount']];
+const sheetStructure = [
+  ['Art.-Nr.', 'Artikel', 'Kühlung', 'TK/Trockenlager', 'Gesamt'],
+];
 
-export function useFormatCreateAndShareXlsx(inventoryId: string) {
+function useInventoryData(inventoryId: string) {
   const inventories = useSelector(
     (state: RootState) => state.invetoriesReducer,
   );
-
   const name: string = inventories[inventoryId].name;
-
   const items: ItemsState | {} = inventories[inventoryId]?.items || {};
 
-  const formatData = (): string[][] => {
+  return { name, items };
+}
+
+function useFormattedData(items: ItemsState | {}) {
+  return (): string[][] => {
     if (Object.keys(items).length > 0) {
       return [
         ...sheetStructure,
-        ...Object.values(items).map(item => [item.name, item.amountOne]),
+        ...Object.values(items).map(item => [
+          item.productId,
+          item.name,
+          item.amountOne,
+          item.amountTwo,
+          eval(`${item.amountOne} + ${item.amountTwo}`),
+        ]),
       ];
     } else {
       return sheetStructure;
     }
   };
+}
 
-  const createSheet = async (data: string[][]): Promise<string> => {
+function useCreateSheet() {
+  return async (data: string[][], name: string): Promise<string> => {
     try {
+      const templateBinaryContent = await RNFS.readFileAssets(
+        '004_Inventurliste_V_2.4.xlsx',
+        'base64',
+      );
+
+      const templateBuffer = Buffer.from(templateBinaryContent, 'base64');
+
+      const templateWorkbook = XLSX.read(templateBuffer, { type: 'buffer' });
+
+      const sheetName = templateWorkbook.SheetNames[0];
+      const sheet = templateWorkbook.Sheets[sheetName];
+
+      for (const rowData of data.slice(1)) {
+        const artNr = rowData[0];
+        const range = XLSX.utils.decode_range(sheet['!ref']!);
+
+        for (let row = range.s.r + 1; row <= range.e.r; row++) {
+          const cellAddress = XLSX.utils.encode_cell({ r: row, c: 0 });
+          const cellValue = sheet[cellAddress]?.v;
+
+          if (String(cellValue) === artNr) {
+            for (let col = 0; col < rowData.length; col++) {
+              const cellAddress = XLSX.utils.encode_cell({ r: row, c: col });
+              sheet[cellAddress] = { t: 's', v: rowData[col] };
+            }
+          }
+        }
+      }
+
       const fileName = `${name}_${moment()
         .format('YYYY_MM_DD_HH_mm')
         .toString()}.xlsx`;
-
       const filePathStr = `${RNFS.CachesDirectoryPath}/${fileName}`;
-
-      const ws = XLSX.utils.aoa_to_sheet(data);
-      const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-      const wbout = XLSX.write(wb, { type: 'binary', bookType: 'xlsx' });
-      await RNFS.writeFile(filePathStr, wbout, 'ascii');
+      const wbout = XLSX.write(templateWorkbook, {
+        type: 'buffer',
+        bookType: 'xlsx',
+      });
+      await RNFS.writeFile(
+        filePathStr,
+        Buffer.from(wbout).toString('base64'),
+        'base64',
+      );
 
       return filePathStr;
     } catch (e: any) {
+      console.error(e);
       return '';
     }
   };
+}
 
-  const grantPermission = async () => {
+function useGrantPermission() {
+  return async () => {
     let isPermitedExternalStorage = await PermissionsAndroid.check(
       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
     );
 
     if (!isPermitedExternalStorage) {
-      // const granted =
       await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
         {
@@ -70,26 +114,31 @@ export function useFormatCreateAndShareXlsx(inventoryId: string) {
           buttonPositive: 'OK',
         },
       );
-
-      // if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
-      //   throw new Error('Permission not granted!');
-      // }
     }
   };
+}
 
-  const shareFile = async (filePath: string) => {
+function useShareFile() {
+  return async (filePath: string) => {
     try {
-      await grantPermission();
+      await useGrantPermission()();
 
       await Share.open({
         url: `file://${filePath}`,
       });
     } catch (error) {}
   };
+}
+
+export function useFormatCreateAndShareXlsx(inventoryId: string) {
+  const { name, items } = useInventoryData(inventoryId);
+  const formatData = useFormattedData(items);
+  const createSheet = useCreateSheet();
+  const shareFile = useShareFile();
 
   async function formatCreateAndShare() {
     const data = formatData();
-    const path = await createSheet(data);
+    const path = await createSheet(data, name);
     await shareFile(path);
   }
 
