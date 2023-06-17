621cbb3d163860c8e96fd0d6458923897e288222
heitzlki <kirill.heitzler@gmail.com>
2023-06-18 01:20:47 +0200

    add firebase and auth

--- client/Inventory/App.tsx
+++ client/Inventory/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './src/config/firebase';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
---
+++ client/Inventory/src/hooks/useAuth.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+const auth = getAuth();
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const unsubscribeFromAuthStateChanged = onAuthStateChanged(auth, user => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        setUser(user);
+      } else {
+        // User is signed out
+        setUser(undefined);
+      }
+    });
+
+    return unsubscribeFromAuthStateChanged;
+  }, []);
+
+  return {
+    user,
+  };
+};
